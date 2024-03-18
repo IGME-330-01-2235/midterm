331b42c26b483367ae8ab077f7613a9ca412431c
Joe Pietruch <jcpigm@rit.edu>
2024-03-17 23:50:59 -0400

    Step 4

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,37 @@ canvas.height = 600;
 // Step 3 - Sign Your Name
 const name = document.querySelector('#studentName') as HTMLParagraphElement;
 name.innerText = 'Joe Pietruch';
+
+// Step 4 - Draw the Canvas
+
+// get the context
+const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+// clear the background
+context.fillStyle = 'white';
+context.fillRect(0, 0, 600, 600);
+
+// draw the circle
+context.translate(300, 300);
+context.strokeStyle = 'black';
+context.lineWidth = 1;
+context.arc(0, 0, 75, 0, 2 * Math.PI);
+context.stroke();
+
+// draw the text
+context.translate(25, 20);
+context.fillStyle = 'black';
+context.font = '14px Arial';
+context.fillText('A', 0, 0);
+
+context.rotate(Math.PI / 2);
+context.font = '28px Arial';
+context.fillText('A', 0, 0);
+
+context.rotate(Math.PI / 2);
+context.font = '56px Arial';
+context.fillText('A', 0, 0);
+
+context.rotate(Math.PI / 2);
+context.font = '112px Arial';
+context.fillText('A', 0, 0);
